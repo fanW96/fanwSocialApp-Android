ec905e3d2a7e9f9986c8f0f97fe1960c1ac8cfb3
fanw96 <fanw9611@qq.com>
2018-03-31 20:23:53 +0800

    实现了分页功能和部分bug的修复

---
+++ app/src/main/java/com/fanw/fanwsocialapp/activity/PersonModifyActivity.java
@@ -0,0 +1,15 @@
+package com.fanw.fanwsocialapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fanw.fanwsocialapp.R;
+
+public class PersonModifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_modify);
+    }
+}
